a1d10d4e11292779c6d72ff978ae8bf93abb4c68
phToan <phamhaitoan10@gmail.com>
2022-06-03 16:30:12 +0700

    middle and models

---
+++ server/models/Cineplexs.js
@@ -0,0 +1,37 @@
+const db = require("../config/db");
+const Sequelize = require("sequelize");
+
+const Cineplexs = db.define("cineplexs", {
+  cineplex_id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  image:{
+    type: Sequelize.TEXT,
+    allowNull:false
+  },
+  googlemapurl: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  createat:{
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+    allowNull:false
+  },
+  updateat:{
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+    allowNull:false
+  }
+});
+
+module.exports = Cineplexs
---
+++ server/models/User.js
@@ -0,0 +1,67 @@
+const db = require("../config/db");
+
+const Sequelize = require("sequelize");
+
+const User = db.define("c_user", {
+  id:{
+    type: Sequelize.INTEGER,
+    primaryKey: true 
+  },
+  role:{
+    type: Sequelize.String,
+    default: "Basic",
+    required: true,
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    minlength: 6
+  },
+
+  fullname: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: Sequelize.INTEGER,
+    allowNull:true,
+    unique: true
+  },
+  birthday: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  avata: {
+    type: Sequelize.TEXT,
+    allowNull: true
+  },
+  status: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  resetpasswordtoken: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  createat:{
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+    allowNull:false
+  },
+  updateat:{
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+    allowNull:false
+  }
+});
+
+module.exports = User
